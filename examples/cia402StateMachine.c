56b0f3d2d0c2cd5bd54a7da306b54d031eeaac5f
neon <oo0-.-0oo@outlook.com>
2025-12-20 16:03:13 +0800

    examples: 增加模拟CiA402的示例

---
+++ examples/cia402StateMachine.c
@@ -0,0 +1,55 @@
+/**
+  ******************************************************************************
+  * File Name          : cia402StateMachine.c
+  * Description        : source for template of cia402 state machine
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) lib3yu(neon).
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+***/
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* Includes ------------------------------------------------------------------*/
+#include "template.h"
+/* Private includes ----------------------------------------------------------*/
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+
+/* Public define 0 -----------------------------------------------------------*/
+/* Public macro 0 ------------------------------------------------------------*/
+/* Public typedef ------------------------------------------------------------*/
+/* Public variables ----------------------------------------------------------*/
+/* Public define 1 -----------------------------------------------------------*/
+/* Public macro 1 ------------------------------------------------------------*/
+/* Public variables ----------------------------------------------------------*/
+/* Public function prototypes ------------------------------------------------*/
+/* Public define 2 -----------------------------------------------------------*/
+/* Public macro 2 ------------------------------------------------------------*/
+/* Public application code ---------------------------------------------------*/
+
+/* Entry point ---------------------------------------------------------------*/
+int main(int argc, char const *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Hello World!\n");    
+    return 0;
+}
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
