6fcd8a5cca8339bb5985ce9a29aca983c5a48c31
neon <oo0-.-0oo@outlook.com>
2025-12-20 14:01:29 +0800

    chore: 增加模板文件

---
+++ examples/template.h
@@ -0,0 +1,51 @@
+/**
+  ******************************************************************************
+  * File Name          : template.h
+  * Description        : Header for template
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) lib3yu(neon).
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+***/
+#ifndef USERAPP_TEMPLATE_H
+#define USERAPP_TEMPLATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Private includes ----------------------------------------------------------*/
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+
+/* Private define 0 ----------------------------------------------------------*/
+/* Private macro 0 -----------------------------------------------------------*/
+/* Public typedef ------------------------------------------------------------*/
+/* Public variables ----------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* Private define 1 ----------------------------------------------------------*/
+/* Private macro 1 -----------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private define 2 ----------------------------------------------------------*/
+/* Private macro 2 -----------------------------------------------------------*/
+/* Private application code --------------------------------------------------*/
+/* Exported application code -------------------------------------------------*/
+/* Public application code ---------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* USERAPP_TEMPLATE_H */
+
